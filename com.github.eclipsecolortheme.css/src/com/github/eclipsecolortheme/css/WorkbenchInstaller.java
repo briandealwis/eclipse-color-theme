614ba2bafce67fd76a048ae898570406d4de36a7
Brian de Alwis <bsd@acm.org>
2014-11-11 15:13:17 -0500

    Use the theme-change event to style changes on the IWorkbench instance to avoid triggering visual artifacts when adding a CSSEngine to the IThemeEngine (seemingly resulting from call to reset the current theme)

--- com.github.eclipsecolortheme.css/src/com/github/eclipsecolortheme/css/WorkbenchInstaller.java
+++ com.github.eclipsecolortheme.css/src/com/github/eclipsecolortheme/css/WorkbenchInstaller.java
@@ -1,37 +1,70 @@
 package com.github.eclipsecolortheme.css;
 
+import java.util.Hashtable;
+
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
 
 import com.github.eclipsecolortheme.css.engine.CSSWorkbenchEngine;
 
 public class WorkbenchInstaller implements IStartup {
 	private IWorkbench workbench;
+	private IThemeEngine themeEngine;
 	private CSSWorkbenchEngine cssEngine;
 	private IExtensionRegistry extensions;
+	private ServiceRegistration<?> registration;
+
+	private EventHandler themeChangedHandler = new EventHandler() {
+		public void handleEvent(Event event) {
+			// prepare for the possibility of multiple workbenches
+			if (themeEngine == event
+					.getProperty(IThemeEngine.Events.THEME_ENGINE)) {
+				themeEngine.applyStyles(workbench, true);
+			}
+		}
+	};
 
 	public void earlyStartup() {
 		workbench = PlatformUI.getWorkbench();
-		extensions = (IExtensionRegistry) workbench
-				.getService(IExtensionRegistry.class);
 
-		final IThemeEngine themeEngine = (IThemeEngine) workbench
-				.getService(IThemeEngine.class);
+		themeEngine = (IThemeEngine) workbench.getService(IThemeEngine.class);
 		if (themeEngine == null) {
 			return;
 		}
 
-		cssEngine = new CSSWorkbenchEngine(workbench, extensions);
+		Bundle bundle = FrameworkUtil.getBundle(getClass());
+		BundleContext context;
+		if (bundle != null && (context = bundle.getBundleContext()) != null) {
+			// not sure what to do if the bundle is null
+			String[] topics = new String[] { IThemeEngine.Events.THEME_CHANGED };
+			Hashtable ht = new Hashtable();
+			ht.put(EventConstants.EVENT_TOPIC, topics);
+			registration = context.registerService(
+					EventHandler.class.getName(), themeChangedHandler, ht);
+		}
+		// FIXME: should add a workbench shutdown listener on the workbench to
+		// remove the service registration
+
 		// FIXME: IThemeEngine does a reset-theme and the SWT CSS engines
 		// assume the reset-theme happens on the SWT thread.
-		workbench.getDisplay().asyncExec(new Runnable() {
-			public void run() {
-				themeEngine.addCSSEngine(cssEngine);
-				// cssEngine.reapply();
-			}
-		});
+		// IExtensionRegistry extensions = (IExtensionRegistry)
+		// workbench.getService(IExtensionRegistry.class);
+		// cssEngine = new CSSWorkbenchEngine(workbench, extensions);
+		// workbench.getDisplay().asyncExec(new Runnable() {
+		// public void run() {
+		// themeEngine.addCSSEngine(cssEngine);
+		// // cssEngine.reapply();
+		// }
+		// });
 	}
 }
--- com.github.eclipsecolortheme.css/src/com/github/eclipsecolortheme/css/engine/WorkbenchThemePropertyHandler.java
+++ com.github.eclipsecolortheme.css/src/com/github/eclipsecolortheme/css/engine/WorkbenchThemePropertyHandler.java
@@ -6,22 +6,37 @@ import java.nio.charset.StandardCharsets;
 
 import org.eclipse.e4.ui.css.core.dom.properties.ICSSPropertyHandler;
 import org.eclipse.e4.ui.css.core.engine.CSSEngine;
+import org.eclipse.ui.IWorkbench;
 import org.w3c.dom.css.CSSPrimitiveValue;
 import org.w3c.dom.css.CSSValue;
 import org.w3c.dom.css.CSSValueList;
+
+import com.github.eclipsecolortheme.ColorThemeApplicator;
+import com.github.eclipsecolortheme.ColorThemeManager;
+import com.github.eclipsecolortheme.ParsedTheme;
 
 public class WorkbenchThemePropertyHandler implements ICSSPropertyHandler {
 
 	public boolean applyCSSProperty(Object element, String property,
 			CSSValue value, String pseudo, CSSEngine engine) throws Exception {
-		assert engine instanceof CSSWorkbenchEngine;
+		// assert engine instanceof CSSWorkbenchEngine;
+
+		IWorkbench workbench;
+		if (element instanceof IWorkbench) {
+			workbench = (IWorkbench) element;
+		} else if (element instanceof WorkbenchElement) {
+			workbench = (IWorkbench) ((WorkbenchElement) element)
+					.getNativeWidget();
+		} else {
+			return false;
+		}
 		if (value instanceof CSSPrimitiveValue
 				&& ((CSSPrimitiveValue) value).getPrimitiveType() == CSSPrimitiveValue.CSS_URI) {
 			InputStream stream = engine.getResourcesLocatorManager()
 					.getInputStream(
 							((CSSPrimitiveValue) value).getStringValue());
 			try {
-				applyEclipseColorTheme(stream, (CSSWorkbenchEngine) engine);
+				applyEclipseColorTheme(workbench, stream, engine);
 			} finally {
 				stream.close();
 			}
@@ -30,7 +45,7 @@ public class WorkbenchThemePropertyHandler implements ICSSPropertyHandler {
 				&& ((CSSPrimitiveValue) value).getPrimitiveType() == CSSPrimitiveValue.CSS_STRING) {
 			InputStream stream = new ByteArrayInputStream(value.getCssText()
 					.getBytes(StandardCharsets.UTF_8));
-			applyEclipseColorTheme(stream, (CSSWorkbenchEngine) engine);
+			applyEclipseColorTheme(workbench, stream, engine);
 			return true;
 		} else if (value.getCssValueType() == CSSValue.CSS_VALUE_LIST) {
 			StringBuilder contents = new StringBuilder();
@@ -46,14 +61,25 @@ public class WorkbenchThemePropertyHandler implements ICSSPropertyHandler {
 			}
 			InputStream stream = new ByteArrayInputStream(contents.toString()
 					.getBytes(StandardCharsets.UTF_8));
-			applyEclipseColorTheme(stream, (CSSWorkbenchEngine) engine);
+			applyEclipseColorTheme(workbench, stream, engine);
 			return true;
 		}
 		return false;
 	}
 
-	private void applyEclipseColorTheme(InputStream stream, CSSWorkbenchEngine engine) {
-		engine.applyEclipseColorTheme(stream);
+	private void applyEclipseColorTheme(IWorkbench workbench,
+			InputStream stream, CSSEngine engine) {
+		// engine.applyEclipseColorTheme(stream);
+		ColorThemeManager manager = new ColorThemeManager();
+		ParsedTheme theme;
+		try {
+			theme = manager.parseTheme(stream, true);
+			new ColorThemeApplicator(workbench, manager).apply(
+					theme.getTheme(), true);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public String retrieveCSSProperty(Object element, String property,
